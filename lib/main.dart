83e3491ac0ad2ba9eeee51e21abf248fd51e6900
flutfix <flutfix@yandex.ru>
2022-02-09 18:03:59 +0300

    Доработки уведомлений

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,17 @@ import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'config/config.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await _init();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
