acab1b42d0a308fab6a94c90f0de4c6a2f0bd168
flutfix <flutfix@yandex.ru>
2022-02-12 19:00:10 +0300

    Рефакторинг расположения папок страницы "Еще"

---
+++ lib/screens/more/pages/inactive_customers/inactive_customers.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fox_fit/controllers/general_cotroller.dart';
+import 'package:fox_fit/utils/error_handler.dart';
+import 'package:get/get.dart';
+
+class InactiveCustomersPage extends StatefulWidget {
+  const InactiveCustomersPage({Key? key}) : super(key: key);
+
+  @override
+  _InactiveCustomersPageState createState() => _InactiveCustomersPageState();
+}
+
+class _InactiveCustomersPageState extends State<InactiveCustomersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
